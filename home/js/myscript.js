17b17f97d5ab69d02ae46e51f31f8e43b5c6751a
ArcielleGercan <acgercan@student.apc.edu.ph>
2024-12-16 15:00:39 +0800

    personal website

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -4,6 +4,6 @@ function myFunction() {
 
 function myFunction2() {
   const element = document.getElementsByTagName("content");
-  document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+  document.getElementById("demo").innerHTML = 'My Personal Website ' + element[0].innerHTML;
 
 }
