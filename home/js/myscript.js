ca19261fce67b13bc72eddfc6c62e8af0bd60536
ArcielleGercan <acgercan@student.apc.edu.ph>
2024-12-16 14:52:06 +0800

    trying to fix

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,9 +1,9 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
 
-  function myFunction2() {
-    const element = document.getElementsByTagName("p");
-    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+function myFunction2() {
+  const element = document.getElementsByTagName("content");
+  document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
 
-  }
+}
