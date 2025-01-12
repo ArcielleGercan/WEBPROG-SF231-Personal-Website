cacf281edab4ad14e4e20594d08510785e3261dc
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-01-13 00:15:45 +0800

    edit comment

--- home/js/comment.js
+++ home/js/comment.js
@@ -1,35 +1,36 @@
 const app2 = Vue.createApp({
     data() {
       return {
-        post: {
-          id: 1,
-          title: "Reactions & Thoughts Welcome!",
-          content: "Hi! I hope you enjoy exploring my personal website. Feel free to leave a reaction and share your thoughts in the comment section below. Thank you for visiting!",
-          reactions: {
-            like: 0,
-            love: 0,
-            haha: 0,
-            wow: 0,
-            sad: 0,
-            angry: 0,
-          },
-          comments: [],
-        },
-        newComment: "",
+        name: '', 
+        newComment: '',
+        comments: [],
+        reactions: {
+          like: 0,
+          love: 0,
+          haha: 0
+        }
       };
     },
     methods: {
       react(type) {
-        this.post.reactions[type]++;
+        this.reactions[type]++;
       },
       addComment() {
-        if (this.newComment.trim() !== "") {
-          this.post.comments.push({ text: this.newComment, timestamp: new Date() });
-          this.newComment = "";
+        if (this.name.trim() && this.newComment.trim()) {
+          const timestamp = new Date().toLocaleString();
+          this.comments.push({
+            name: this.name,
+            text: this.newComment,
+            timestamp: timestamp
+          });
+          this.newComment = '';
+          this.name = ''; 
+        } else {
+          alert('Please enter both a name and a comment!');
         }
-      },
-    },
+      }
+    }
   });
-
+  
   app2.mount('#app2');
   
