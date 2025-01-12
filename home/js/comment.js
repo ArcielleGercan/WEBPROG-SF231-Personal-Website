59965af4e9b4e6f8dd15195038cca7184595138e
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-01-13 00:01:06 +0800

    edit comment section

---
+++ home/js/comment.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        post: {
+          id: 1,
+          title: "Reactions & Thoughts Welcome!",
+          content: "Hi! I hope you enjoy exploring my personal website. Feel free to leave a reaction and share your thoughts in the comment section below. Thank you for visiting!",
+          reactions: {
+            like: 0,
+            love: 0,
+            haha: 0,
+            wow: 0,
+            sad: 0,
+            angry: 0,
+          },
+          comments: [],
+        },
+        newComment: "",
+      };
+    },
+    methods: {
+      react(type) {
+        this.post.reactions[type]++;
+      },
+      addComment() {
+        if (this.newComment.trim() !== "") {
+          this.post.comments.push({ text: this.newComment, timestamp: new Date() });
+          this.newComment = "";
+        }
+      },
+    },
+  });
+  
